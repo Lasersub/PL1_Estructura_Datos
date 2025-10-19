8c39adecc5d2d5b3034f8b6022d1a26ced9bcb5f
RayanBZaki <rayan.bentaleb@edu.uah.es>
2025-10-19 13:31:22 +0200

    Editorial.h con definicion de pila y cola prematura
    Todavia queda modificar mucho del .h

--- include/editorial.h
+++ include/editorial.h
@@ -3,9 +3,63 @@
 
 #include <iostream>
 #include <string>
-#include <cstdlib>
-#include <ctime>
 
 
-#endif // EDITORIAL_H
+class NodoPila
+{
+private:
+    int valor;
+    NodoPila *siguiente;
+    friend class Pila;
+public:
+    NodoPila();
+    NodoPila(int v, NodoPila *sig = NULL);
+    ~NodoPila();
+};
+
+typedef NodoPila *pnodo;
+
+
+class Pila
+{
+private:
+    pnodo cima;
+public:
+    Pila();
+    ~Pila();
+    bool esVacia();
+    void apilar(int v);
+    void desapilar();
+    int mostrar();
+};
+
+class NodoCola
+{
+    friend class Cola;
+private:
+    NodoCola *siguiente;
+    char elemento;
+public:
+    NodoCola();
+    NodoCola(char e, NodoCola*sig = NULL);
+    ~NodoCola();
+};
+
+
+class Cola
+{
+private:
+    NodoCola * primero;
+    NodoCola * ultimo;
+    int longitud;
+public:
+    Cola();
+    ~Cola();
+    void encolar(char);
+    char inicio();
+    char fin();
+    char desencolar();
+    bool es_vacia();
+    void mostrarCola(); //No es correcto, se implementa para ver que está creada correctamente
+};
 
