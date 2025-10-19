fcd314744937589f46cd2ea72da06e2045fa4da4
Óscar López Rojo <oscar.lopezrojo@edu.uah.es>
2025-10-19 14:02:16 +0200

    Errores eliminados en los cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include "editorial.h"
-#include "editorial.cpp"
 
 using namespace std;
+
+// Muestra el menú al usuario
+void mostrar_menu()
+{
+    cout << "\n=======================================================" << endl;
+    cout << "   SIMULACION DE PEDIDOS EN EDITORIAL" << endl;
+    cout << "=========================================================" << endl;
+    cout << "1. Generar N pedidos aleatorios" << endl;
+    cout << "2. Ejecutar un paso de simulacion" << endl;
+    cout << "3. Mostrar el estado del sistema" << endl;
+    cout << "4. Ver el contenido de una caja de una libreria" << endl;
+    cout << "0. Salir del programa" << endl;
+    cout << "=========================================================" << endl;
+    cout << "Seleccione una opcion: ";
+}
 
 /**
  * @brief Función principal que ejecuta el simulador.
  */
-int main() {
-    mostrar_menu()
+int main()
+{
+    mostrar_menu();
 
     return 0;
 }
+
+
+
